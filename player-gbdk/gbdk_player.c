a1ed65d6af14293ac96912501f8f192f87fffa74
toxa <untoxa@mail.ru>
2020-08-24 15:43:20 +0300

    C API comments; channel muting demo in gbdk_player.c

--- player-gbdk/gbdk_player.c
+++ player-gbdk/gbdk_player.c
@@ -1,4 +1,6 @@
 #include <gb/gb.h>
+#include <stdio.h>
+#include <gb/console.h>
 #include "hUGEDriver.h"
 
 extern void Intro[];
@@ -6,23 +8,28 @@ extern void Intro[];
 UBYTE joy;
 UBYTE c0 = 0, c1 = 0, c2 = 0, c3 = 0;
 
+const unsigned char * const statstr[2] = {"active", "mute   "};
+
 void main() {
     NR52_REG = 0x80;
     NR51_REG = 0xFF;
     NR50_REG = 0x77;
 
-    hUGE_init(Intro);
+    puts("ch1: active\nch2: active\nch3: active\nch4: active\n");
+
     __critical {
+        hUGE_init(Intro);
         add_VBL(hUGE_dosound);
     }
+        
     while(1) {
         wait_vbl_done();
         joy = joypad();
         switch (joy) {
-            case J_UP    : c0 ^= 1; hUGE_mute_channel(0, c0); waitpadup(); break;
-            case J_DOWN  : c1 ^= 1; hUGE_mute_channel(1, c1); waitpadup(); break;
-            case J_LEFT  : c2 ^= 1; hUGE_mute_channel(2, c2); waitpadup(); break;
-            case J_RIGHT : c3 ^= 1; hUGE_mute_channel(3, c3); waitpadup(); break;
+            case J_UP    : c0 ^= 1; hUGE_mute_channel(0, c0); gotoxy(5, 0); puts(statstr[c0]); waitpadup(); break;
+            case J_DOWN  : c1 ^= 1; hUGE_mute_channel(1, c1); gotoxy(5, 1); puts(statstr[c1]); waitpadup(); break;
+            case J_LEFT  : c2 ^= 1; hUGE_mute_channel(2, c2); gotoxy(5, 2); puts(statstr[c2]); waitpadup(); break;
+            case J_RIGHT : c3 ^= 1; hUGE_mute_channel(3, c3); gotoxy(5, 3); puts(statstr[c3]); waitpadup(); break;
         }
     }
 }
--- player-gbdk/hUGEDriver.h
+++ player-gbdk/hUGEDriver.h
@@ -1,8 +1,13 @@
 #ifndef HUGEDRIVER_H_INCLUDE
 #define HUGEDRIVER_H_INCLUDE
 
+// initialize the driver with song data
 void hUGE_init(void * song);
+
+// driver routine
 void hUGE_dosound();
-void hUGE_mute_channel(unsigned char ch, unsigned char enable);
+
+// mute/unmute channel; ch: is a channel number 0..3, mute: 1 to mute, 0 to unmute.
+void hUGE_mute_channel(unsigned char ch, unsigned char mute);
 
 #endif
