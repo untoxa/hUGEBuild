d5e65a592744c9f4d5238bd850fdd63af48b0f5c
Toxa <untoxa@mail.ru>
2023-01-23 01:23:44 +0300

    tidy up RGB2SDAS code

--- tools/rgb2sdas.py
+++ tools/rgb2sdas.py
@@ -19,89 +19,97 @@ class RGBObject(object):
         self.Nodes = []
 
         offset = 0
-        self.id, self.rev = unpack_from('<4si', data, offset); offset += calcsize('<4si')
+        self.id, self.rev, offset = self.read_record('<4si', data, offset)
         if (self.id != b'RGB9') or (self.rev != 8): raise Exception("RGBDS Object version mismatch!")
 
-        nSymbols, nSections, nNodes = unpack_from('<iii', data, offset); offset += calcsize('<iii')
+        nSymbols, nSections, nNodes, offset = self.read_record('<iii', data, offset)
 
         # read Nodes
         for i in range(nNodes):
-            parentid, parentlineno, nodetype  = unpack_from('<iiB', data, offset); offset += calcsize('<iiB')
+            parentid, parentlineno, nodetype, offset = self.read_record('<iiB', data, offset)
             iter = [];
             name = ''
             if (nodetype != 0):
-                name, sz = self.read_null_term(data, offset); offset += sz
+                name, offset = self.read_null_term(data, offset)
             else:
-                depth = unpack_from('<i', data, offset)[0]; offset += calcsize('i')
-                iter, sz = self.read_array('<i', depth, data, offset); offset += sz
+                depth, offset = self.read_record('<i', data, offset)
+                iter, offset = self.read_array('<i', depth, data, offset)
 
             self.Nodes.append({'ParentID': parentid, 'ParentLineNo': parentlineno, 'NodeType': nodetype, 'Name': name, 'Iter': iter})
 
         # read Symbols
         for i in range(nSymbols):
-            name, sz = self.read_null_term(data, offset); offset += sz
-            symtype = unpack_from('B', data, offset)[0]; offset += calcsize('B')
+            name, offset = self.read_null_term(data, offset)
+            symtype, offset = self.read_record('B', data, offset)
             if ((symtype & 0x7f) != SYM_IMPORT):
-                sourcefile, linenum, sectionid, value = unpack_from('<iiii', data, offset); offset += calcsize('<iiii')
+                sourcefile, linenum, sectionid, value, offset = self.read_record('<iiii', data, offset)
                 self.Symbols.append({'Id': i, 'Name': name, 'SymType': symtype, 'SourceFile': sourcefile, 'LineNum': linenum, 'SectionId': sectionid, 'Value': value})
             else:
                 self.Symbols.append({'Id': i, 'Name': name, 'SymType': symtype})
 
         # read Sections
         for i in range(nSections):
-            name, sz = self.read_null_term(data, offset); offset += sz
-            size, secttype, org, bank, align, ofs = unpack_from('<iBiiBi', data, offset); offset += calcsize('<iBiiBi')
+            name, offset = self.read_null_term(data, offset)
+            size, secttype, org, bank, align, ofs, offset = self.read_record('<iBiiBi', data, offset)
 
             sect = {'Id': i, 'Name': name, 'Size': size, 'SectType': secttype, 'Org': org, 'Bank': bank, 'Align': align, 'Ofs': ofs}
             self.Sections.append(sect)
 
             if (sect['SectType'] in [ROM0, ROMX]):
-                sect['Data'] = data[offset:offset+size]; offset += size
-                npatches = unpack_from('<i', data, offset)[0]; offset += calcsize('i')
+                sect['Data'], offset = self.read_blob(data, offset, size)
+                npatches, offset = self.read_record('<i', data, offset)
                 sect['Patches'] = patches = []
                 for j in range(npatches):
-                    sourcefile, linenum, ofs, pcsectionid, pcoffset, patchtype, rpnsize = unpack_from('<iiiiiBi', data, offset); offset += calcsize('<iiiiiBi')
-                    rpn = self.decode_rpn(data[offset:offset+rpnsize]); offset += rpnsize
+                    sourcefile, linenum, ofs, pcsectionid, pcoffset, patchtype, rpnsize, offset = self.read_record('<iiiiiBi', data, offset)
+                    rpndata, offset = self.read_blob(data, offset, rpnsize)
+                    rpn = self.decode_rpn(rpndata)
                     patches.append({'SourceFile': sourcefile, 'LineNum': linenum, 'Offset': ofs, 'PCSectionId': pcsectionid, 'PCOffset': pcoffset, 'PatchType': patchtype, 'RPN': rpn})
+
+    def read_record(self, fmt, data, offset):
+        return (*unpack_from(fmt, data, offset), offset + calcsize(fmt))
 
     def read_null_term(self, data, offset):
         j = 0
         while (data[offset + j] != 0): j += 1
-        return (data[offset:offset + j].decode('ansi'), j + 1)
+        return (data[offset:offset + j].decode('ansi'), offset + j + 1)
 
     def read_array(self, fmt, count, data, offset):
         sz = calcsize(fmt)
         res = []
         for j in range(count):
-            res.append(unpack_from(fmt, data, offset)); offset += sz
-        return (res, sz * count)
+            res.append(unpack_from(fmt, data, offset))
+            offset += sz
+        return (res, offset)
+
+    def read_blob(self, data, offset, size):
+        return (data[offset:offset+size], offset + size)
 
     def decode_rpn(self, data):
         result = []
-        i = 0
-        while (i < len(data)):
+        read_position = 0
+        while (read_position < len(data)):
             itm = {}
-            itm['Tag'] = tag = data[i]; i += 1
+            itm['Tag'] = tag = data[read_position]; read_position += 1
             if tag == rpnBankSymbol:
-                itm['BankSymbol'], sz = self.read_null_term(data, i); i += sz
+                itm['BankSymbol'], read_position = self.read_null_term(data, read_position)
             elif tag == rpnBankSection:
-                itm['BankSection'], sz = self.read_null_term(data, i); i += sz
+                itm['BankSection'], read_position = self.read_null_term(data, read_position)
             elif tag == rpnSizeOfSection:
-                itm['SizeOfSection'], sz = self.read_null_term(data, i); i += sz
+                itm['SizeOfSection'], read_position = self.read_null_term(data, read_position)
             elif tag == rpnStartOfSection:
-                itm['StartOfSection'], sz = self.read_null_term(data, i); i += sz
+                itm['StartOfSection'], read_position = self.read_null_term(data, read_position)
             elif tag == rpnInteger:
-                itm['IntValue'] = unpack_from('<i', data, i)[0]; i += calcsize('i')
+                itm['IntValue'], read_position = self.read_record('<i', data, read_position)
             elif tag == rpnSymbol:
-                itm['SymbolId'] = unpack_from('<i', data, i)[0]; i += calcsize('i')
+                itm['SymbolId'], read_position = self.read_record('<i', data, read_position)
             result.append(itm)
         return result
 
     def find_patch(self, section, index):
         for patch in section['Patches']:
             if (patch['Offset'] == index):
-                return True, patch
-        return False, {}
+                return (True, patch)
+        return (False, {})
 
     def log(self):
         print("ID: {:s} REV: {:d}".format(self.id.decode('ansi'), self.rev))
@@ -217,15 +225,16 @@ def main(argv=None):
         for section in obj.Sections:
             if (section['SectType'] != ROM0) and (section['SectType'] != ROMX):
                 continue
+
             data = section.setdefault('Data', [])
             if (len(data) == 0):
                 continue;
 
-            PC = 0
-            while PC < len(data):
-                write_pos = PC + (section['Org'] if (section['Org'] != -1) else 0)
+            read_position = 0
+            while read_position < len(data):
+                PC = read_position + (section['Org'] if (section['Org'] != -1) else 0)
 
-                res, patch = obj.find_patch(section, PC)
+                res, patch = obj.find_patch(section, read_position)
                 if (res):
                     RPN = patch['RPN']
                     if (patch['PatchType'] == PATCH_BYTE):
@@ -236,12 +245,12 @@ def main(argv=None):
 
                         symbol = obj.Symbols[RPN[0]['SymbolId']]
                         if len(RPN) == 3:
-                            f.write(bytes('T {:02X} {:02X} 00 {:02X} {:02X} 00\n'.format(lo(write_pos), hi(write_pos), lo(symbol['Value']), hi(symbol['Value'])), 'ascii'))
+                            f.write(bytes('T {:02X} {:02X} 00 {:02X} {:02X} 00\n'.format(lo(PC), hi(PC), lo(symbol['Value']), hi(symbol['Value'])), 'ascii'))
                             f.write(bytes('R 00 00 {:02X} {:02X} 09 03 {:02X} {:02X}\n'.format(lo(section['Id']), hi(section['Id']), lo(symbol['SectionId']), hi(symbol['SectionId'])), 'ascii'))
                         elif len(RPN) == 5:
-                            f.write(bytes('T {:02X} {:02X} 00 {:02X} {:02X} 00\n'.format(lo(write_pos), hi(write_pos), lo(symbol['Value']), hi(symbol['Value'])), 'ascii'))
+                            f.write(bytes('T {:02X} {:02X} 00 {:02X} {:02X} 00\n'.format(lo(PC), hi(PC), lo(symbol['Value']), hi(symbol['Value'])), 'ascii'))
                             f.write(bytes('R 00 00 {:02X} {:02X} 89 03 {:02X} {:02X}\n'.format(lo(section['Id']), hi(section['Id']), lo(symbol['SectionId']), hi(symbol['SectionId'])), 'ascii'))
-                        PC += 1
+                        read_position += 1
                     elif (patch['PatchType'] == PATCH_LE_WORD):
                         if (((len(RPN) == 3) and ((RPN[1]['Tag'] != rpnInteger) or (RPN[2]['Tag'] != rpnPlus))) or
                             (not len(RPN) in [1, 3])):
@@ -256,26 +265,26 @@ def main(argv=None):
                         if (symbol['SymType'] == SYM_IMPORT):
                             if (symbol['No'] < 0):
                                 raise Exception('Trying to reference eliminated symbol');
-                            f.write(bytes('T {:02X} {:02X} 00 {:02X} {:02X}\n'.format(lo(write_pos), hi(write_pos), lo(value_to_write), hi(value_to_write)), 'ascii'))
+                            f.write(bytes('T {:02X} {:02X} 00 {:02X} {:02X}\n'.format(lo(PC), hi(PC), lo(value_to_write), hi(value_to_write)), 'ascii'))
                             f.write(bytes('R 00 00 {:02X} {:02X} 02 03 {:02X} {:02X}\n'.format(lo(section['Id']), hi(section['Id']), lo(symbol['No']), hi(symbol['No'])), 'ascii'))
                         else:
-                            f.write(bytes('T {:02X} {:02X} 00 {:02X} {:02X}\n'.format(lo(write_pos), hi(write_pos), lo(value_to_write), hi(value_to_write)), 'ascii'))
+                            f.write(bytes('T {:02X} {:02X} 00 {:02X} {:02X}\n'.format(lo(PC), hi(PC), lo(value_to_write), hi(value_to_write)), 'ascii'))
                             f.write(bytes('R 00 00 {:02X} {:02X} 00 03 {:02X} {:02X}\n'.format(lo(section['Id']), hi(section['Id']), lo(symbol['SectionId']), hi(symbol['SectionId'])), 'ascii'))
-                        PC += 2
+                        read_position += 2
                     elif (patch['PatchType'] == PATCH_JR):
                         if (len(RPN) != 1):
                             raise Exception('Unsupported RPN expression in JR patch')
 
                         symbol = obj.Symbols[RPN[0]['SymbolId']]
-                        f.write(bytes('T {:02X} {:02X} 00 {:02X}\n'.format(lo(write_pos), hi(write_pos), symbol['Value'] - PC - 1), 'ascii'))
+                        f.write(bytes('T {:02X} {:02X} 00 {:02X}\n'.format(lo(PC), hi(PC), symbol['Value'] - read_position - 1), 'ascii'))
                         f.write(bytes('R 00 00 {:02X} {:02X}\n'.format(lo(section['Id']), hi(section['Id'])), 'ascii'))
-                        PC += 1
+                        read_position += 1
                     else:
                         raise Exception('Unsupported patch type: {:d} Section: {:s}'.format(patch['PatchType'], section['Name']))
                 else:
-                    f.write(bytes('T {:02X} {:02X} 00 {:02X}\n'.format(lo(write_pos), hi(write_pos), data[PC]), 'ascii'))
+                    f.write(bytes('T {:02X} {:02X} 00 {:02X}\n'.format(lo(PC), hi(PC), data[read_position]), 'ascii'))
                     f.write(bytes('R 00 00 {:02X} {:02X}\n'.format(lo(section['Id']), hi(section['Id'])), 'ascii'))
-                    PC += 1
+                    read_position += 1
     print('RGB2SDAS: object {:s} was successfully converted to {:s}'.format(infilename.name, outfilename.name))
 
 if __name__=='__main__':
